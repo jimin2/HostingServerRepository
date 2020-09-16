2410f5f5c80391e41046a8c40faea6f95f4b84d7
Jimin LEE <ljm0717@interpark.com>
2020-09-16 16:43:03 +0900

    jsonServer 변경

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router1 = jsonServer.router('db.json');
-const router2 = jsonServer.router('test.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router1);
-server.use(router2);
+server.use(router);
 
 server.listen(port);
