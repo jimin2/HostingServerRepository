0d77de72e0b9c3b0f24564485b84b8de5dc1b552
Jimin LEE <ljm0717@interpark.com>
2020-09-16 16:40:55 +0900

    jsonServer 추가

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router1 = jsonServer.router('db.json');
+const router2 = jsonServer.router('test.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+server.use(router1);
+server.use(router2);
 
 server.listen(port);
